6029cde6cbdd451a6d2dfef2526732e2feb5fe74
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-12-07 11:43:47 -0800

    12/6

--- Yahtzee.java
+++ Yahtzee.java
@@ -8,26 +8,65 @@ public class Yahtzee
     Die6 dieE;
     int number;
     public Yahtzee() {
-    dieA = new Die6();
-    dieB = new Die6();
-    dieC = new Die6();
-    dieD = new Die6();
-    dieE = new Die6();
+        dieA = new Die6();
+        dieB = new Die6();
+        dieC = new Die6();
+        dieD = new Die6();
+        dieE = new Die6();
     }
+
+    public void roll() {
+        dieA.roll();
+        dieB.roll();
+        dieC.roll();
+        dieD.roll();
+        dieE.roll();
+    }
+
     public void roll(int dieNumber) {
-        d1 = dieA.roll();
-        d2= dieB.roll();
-        d3 = dieC.roll();
-        d4 = dieD.roll();
-        d5 = dieE.roll();
-        number = dieNumber;
-    }
-    public String summarize() {
-        if (number >= 1 || number <=6) {
-            return "1" + "-" + number;
+        if (dieNumber == 1) {
+            dieA.roll();
+        } else if (dieNumber == 2) {
+            dieB.roll();
+        } else if (dieNumber == 3) {
+            dieC.roll(); 
+        } else if (dieNumber == 4) {
+            dieD.roll();
+        } else if (dieNumber == 5) {
+            dieE.roll();
         }
     }
+
+    public int getValue1() {
+        return dieA.getValue();
+    }
+    public int getValue2() {
+        return dieB.getValue();
+    }
+    public int getValue3() {
+        return dieC.getValue();
+    }
+    public int getValue4() {
+        return dieD.getValue();
+    }
+    public int getValue5() {
+        return dieE.getValue();
+    }
+
+    public int rollAndGetValue() {
+        roll();
+        return getValue1();
+    }
+
+    public String summarize() {
+        if (number >= 1 || number <=6) {
+            return "1" + "-" + number + ";" + "2" + "-" + number + ";" + "3" + "-" + number + ";" + "4" + "-" + number + ";" + "1" + "-" + number;
+        } else {
+            return "bruh";
+        }
+    }
+
     public String ToString() {
-        return "Dice values: " + 1 + " " + 2 + " " + 3 + " " + 4 + " " + 5;
+        return "Dice values: " + dieA.getValue() + " " + dieB.getValue() + " " + dieC.getValue() + " " + dieD.getValue() + " " + dieE.getValue();
     }
 }
