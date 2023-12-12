fc07b5673477a916fc1a40697347dd674662844c
Sean Pohto <sean.pohto@mrjaffesclass.com>
2023-12-12 11:44:40 -0800

    12/9

--- Yahtzee.java
+++ Yahtzee.java
@@ -6,16 +6,18 @@ public class Yahtzee
     Die6 die3;
     Die6 die4;
     Die6 die5;
-    int[] dieSet = {die1, die2, die3, die4, die5};
+    int[] dieSet;
     public Yahtzee() {
         die1 = new Die6();
         die2 = new Die6();
         die3 = new Die6();
         die4 = new Die6();
         die5 = new Die6();
+        dieSet[] new Die6() = {die1, die2, die3, die4, die5};
+        this.roll();
     }
 
-    public void allRoll() {
+    public void roll() {
         die1.roll();
         die2.roll();
         die3.roll();
@@ -36,20 +38,20 @@ public class Yahtzee
             die5.roll();
         }
     }
-    public int rollAndGetValue() {
-        allRoll();
-        return getValue1();
-    }
 
     public String summarize() {
         int[] sideArray = {1, 2, 3, 4, 5, 6};
         int[] dieCount = {0, 0, 0, 0, 0, 0};
         for (int x = 0; x < 5; x++){
             for (int y = 0; y < 5; y++) {
-                if (dieSet[x].getValue() == sideArray[y]) {
+                if (dieSet[x] == sideArray[y]) {
                     dieCount[y] = dieCount[y]++;
                 }
             }
+        }
+        String organize = "";
+        for (int i = 1; i<6; i++) {
+            organize = organize + i + "-" + dieCount[i-1] + ";";
         }
     }
 
